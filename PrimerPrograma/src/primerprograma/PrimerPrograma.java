b7e119b6a9b44d5601910ac4cb1f2915a9400fdd
aitorromero <romeroaitor.sgr@gmail.com>
2016-09-15 19:31:32 +0200

    Update PrimerPrograma.java

--- PrimerPrograma/src/primerprograma/PrimerPrograma.java
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -3,6 +3,7 @@ package primerprograma;
 public class PrimerPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");    }
+        System.out.println("Hola Mundo");
+        System.out.println("Alejandro jermozo"); }
     
 }
